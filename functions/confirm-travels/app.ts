8cd46f489c430df0e55695dee5abc890bd622006
aurelien-robineau <robineau.aure@gmail.com>
2025-11-02 10:51:28 +0100

    feat: enhance logging and error handling in SNCF user travel confirmation
    - Added detailed debug logs for user retrieval, travel confirmation, and card processing.
    - Improved error messages to include user names for better traceability.
    - Updated the logging for the SSM user update process.

--- functions/confirm-travels/app.ts
+++ functions/confirm-travels/app.ts
@@ -24,8 +24,11 @@ interface SNCFUser {
  * @returns The SNCF users from SSM.
  */
 async function getUsers(): Promise<SNCFUser[]> {
+  console.debug("Getting SNCF users from SSM...")
+
   const usersAsJSONString = await getParameterValue(SSMConfig.UsersParameterName, true);
   if (!usersAsJSONString) {
+    console.error("Cannot get SNCF users from SSM.");
     throw new Error("Cannot get SNCF users from SSM.");
   }
 
@@ -34,38 +37,49 @@ async function getUsers(): Promise<SNCFUser[]> {
     users = JSON.parse(usersAsJSONString);
   }
   catch (err) {
+    console.error("Cannot parse SNCF users from SSM.");
     throw new Error("Cannot parse SNCF users from SSM.");
   }
 
   if (!Array.isArray(users)) {
+    console.error("SNCF users is not an array.");
     throw new Error("SNCF users is not an array.");
   }
 
   users = users.filter((user) => user.accessToken);
 
+  console.debug(`Found ${users.length} users with access token.`)
+
   return users;
 }
+
 /**
  * Confirms the travels for the SNCF users.
  * @param users - The SNCF users to confirm travels for.
  * @returns The number of confirmed travels for each card number.
  */
 export  async function confirmUserTravels(users: SNCFUser[]): Promise<{ [key: string]: number; }> {
+  console.debug(`Confirming travels for ${users.length} users...`)
+
   const oneDayAgo = new Date(Date.now() - (1000 * 60 * 60 * 24));
   const confirmedTravelsCount: { [key: string]: number } = {};
   
   for (const user of users) {
+    console.debug(`Confirming travels for user: ${user.name}`)
+
     const { accessToken } = user;
     const sncfApi = new SNCFMaxJeuneAPI(accessToken);
 
     let customerInfo;
     try {
+      console.debug(`Retrieving customer info for user: ${user.name}`)
+
       customerInfo = await sncfApi.getCustomerInfo();
       user.accessToken = sncfApi.accessToken;
       user.name = `${customerInfo.firstName} ${customerInfo.lastName}`;
     }
     catch (err: any) {
-      console.error(`Error retrieving customer info for user: ${err.message}`);
+      console.error(`Error retrieving customer info for user ${user.name}: ${err.message}`);
       continue;
     }
 
@@ -79,6 +93,8 @@ export  async function confirmUserTravels(users: SNCFUser[]): Promise<{ [key: st
     }
 
     for (const card of validCards) {
+      console.debug(`Processing card: ${card.cardNumber}`)
+
       const cardNumber = card.cardNumber;
       if (!confirmedTravelsCount[cardNumber]) {
         confirmedTravelsCount[cardNumber] = 0;
@@ -86,6 +102,7 @@ export  async function confirmUserTravels(users: SNCFUser[]): Promise<{ [key: st
 
       let travels: Travel[] = [];
       try {
+        console.debug(`Retrieving travels for card number: ${card.cardNumber}`)
         travels = await sncfApi.getTravels(cardNumber, oneDayAgo);
         user.accessToken = sncfApi.accessToken;
       }
@@ -93,13 +110,20 @@ export  async function confirmUserTravels(users: SNCFUser[]): Promise<{ [key: st
         console.error(`Error retrieving travels for card number ${cardNumber}: ${err.message}`);
         continue;
       }
+
+      console.debug(`Found ${travels.length} travels for card number: ${cardNumber}`)
     
       const travelsToConfirm = travels.filter((travel) => travel.travelConfirmed === "TO_BE_CONFIRMED");
+      console.debug(`Found ${travelsToConfirm.length} travels to confirm for card number: ${cardNumber}`)
+
       if (travelsToConfirm.length === 0) {
+        console.debug(`No travels to confirm for card number: ${cardNumber}`)
         continue;
       }
     
       for (const travel of travelsToConfirm) {
+        console.debug(`Confirming travel: ${travel.orderId} for card number: ${cardNumber}`)
+
         try {
           await sncfApi.confirmTravel(travel);
           user.accessToken = sncfApi.accessToken;
@@ -111,6 +135,8 @@ export  async function confirmUserTravels(users: SNCFUser[]): Promise<{ [key: st
       }
     }
   }
+
+  console.debug(`Updating SNCF users in SSM...`)
 
   updateParameter(SSMConfig.UsersParameterName, JSON.stringify(users)).catch((err) => {
     console.error("Error updating SNCF users in SSM.", err);
@@ -150,6 +176,12 @@ export const lambdaHandler = async (_event: APIGatewayProxyEvent): Promise<APIGa
   const stringifiedConfirmedTravelsCount = Object.entries(confirmedTravelsCount)
     .map(([cardNumber, count]) => `- ${cardNumber}: ${count}`)
     .join("\n");
+
+  console.debug(
+    totalConfirmedTravelsCount === 0
+    ? "No travels confirmed."
+    : `Travels confirmed: ${stringifiedConfirmedTravelsCount}`
+  );
 
   return {
     statusCode: 204,
